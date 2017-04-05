d10632e014e5dcb518ff7d015467c9eedfc3fffc
Christian <christian.garcia@yplanapp.com>
2017-04-05 11:16:30 +0100

    implementation + tests

---
+++ app/src/main/java/com/christiangp/rxjavaplayground/MigrateExample.java
@@ -0,0 +1,65 @@
+package com.christiangp.rxjavaplayground;
+
+import java.util.List;
+
+import io.reactivex.Completable;
+import io.reactivex.Maybe;
+import io.reactivex.Observable;
+
+public class MigrateExample {
+
+    private final DataSourceV1 dataSourceV1;
+
+    private final DataSourceV2 dataSourceV2;
+
+    public MigrateExample(DataSourceV1 dataSourceV1, DataSourceV2 dataSourceV2) {
+        this.dataSourceV1 = dataSourceV1;
+        this.dataSourceV2 = dataSourceV2;
+    }
+
+    public Completable migrate2() {
+        return dataSourceV2.getAll()
+                           .filter(searchesV2 -> !searchesV2.isEmpty())
+                           .switchIfEmpty(dataSourceV1.fetchAllSearchesV1()
+                                                      .flatMapObservable(Observable::fromIterable)
+                                                      .doOnNext(dataSourceV2::save)
+                                                      .ignoreElements()
+                                                      .toMaybe())
+                           .ignoreElement();
+    }
+
+    public Completable migrate() {
+        return dataSourceV2.getAll()
+                           .filter(searchesV2 -> !searchesV2.isEmpty())
+                           .switchIfEmpty(dataSourceV1.fetchAllSearchesV1()
+                                                      .doOnSuccess(searchDBs -> {
+                                                          for (SearchDB searchDB : searchDBs) {
+                                                              dataSourceV2.save(searchDB);
+                                                          }
+                                                      })
+                                                      .ignoreElement()
+                                                      .toMaybe()
+                           )
+                           .ignoreElement();
+    }
+
+    public interface DataSourceV2 {
+
+        Maybe<List<SavedSearch>> getAll();
+
+        void save(SearchDB searchDB);
+    }
+
+    public interface DataSourceV1 {
+
+        Maybe<List<SearchDB>> fetchAllSearchesV1();
+    }
+
+    public static class SearchDB {
+
+    }
+
+    public static class SavedSearch {
+
+    }
+}
---
+++ app/src/test/java/com/christiangp/rxjavaplayground/MigrateExampleTest.java
@@ -0,0 +1,94 @@
+package com.christiangp.rxjavaplayground;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import io.reactivex.Maybe;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.never;
+
+public class MigrateExampleTest {
+
+    private MigrateExample              migrateExample;
+
+    @Mock
+    private MigrateExample.DataSourceV1 v1;
+
+    @Mock
+    private MigrateExample.DataSourceV2 v2;
+
+    @Mock
+    private MigrateExample.SearchDB     searchV1_1;
+
+    @Mock
+    private MigrateExample.SearchDB     searchV1_2;
+
+    @Mock
+    private MigrateExample.SavedSearch  searchV2_1;
+
+    @Before
+    public void setUp()
+            throws Exception {
+        MockitoAnnotations.initMocks(this);
+        migrateExample = new MigrateExample(v1, v2);
+    }
+
+    @Test
+    public void givenV2IsEmpty_WhenMigrate_ThenMigrateFromV1()
+            throws Exception {
+
+        given(v2.getAll()).willReturn(Maybe.empty());
+        given(v1.fetchAllSearchesV1()).willReturn(Maybe.just(Arrays.asList(searchV1_1, searchV1_2)));
+
+        migrateExample.migrate()
+                      .test()
+                      .assertNoErrors()
+                      .assertComplete();
+
+        then(v2).should()
+                .save(searchV1_1);
+        then(v2).should()
+                .save(searchV1_2);
+    }
+
+    @Test
+    public void givenV2HasEmptyElements_WhenMigrate_ThenMigrateFromV1()
+            throws Exception {
+
+        given(v2.getAll()).willReturn(Maybe.just(Collections.emptyList()));
+        given(v1.fetchAllSearchesV1()).willReturn(Maybe.just(Arrays.asList(searchV1_1, searchV1_2)));
+
+        migrateExample.migrate()
+                      .test()
+                      .assertNoErrors()
+                      .assertComplete();
+
+        then(v2).should()
+                .save(searchV1_1);
+        then(v2).should()
+                .save(searchV1_2);
+    }
+
+    @Test
+    public void givenV2HAsItems_WhenMigrate_ThenSaveNothingFromV1()
+            throws Exception {
+        given(v2.getAll()).willReturn(Maybe.just(Collections.singletonList(searchV2_1)));
+        given(v1.fetchAllSearchesV1()).willReturn(Maybe.just(Arrays.asList(searchV1_1, searchV1_2)));
+
+        migrateExample.migrate()
+                      .test()
+                      .assertNoErrors()
+                      .assertComplete();
+
+        then(v2).should(never())
+                .save(any());
+    }
+}
